29228b3d1e7a1b039300e84f8f6bc23dee04f29e
edumueller <eduardo.nicastro@icloud.com>
2021-09-04 20:29:23 -0300

    solved maxChar algo

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,20 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const h = {};
+  for (let i = 0; i <= str.length; i++) {
+    h[str[i]] = h[str[i]] + 1 || 1;
+  }
+
+  let maxI;
+  for (let i in h) {
+    if (!maxI || h[i] > h[maxI]) {
+      maxI = i;
+    }
+  }
+
+  return maxI;
+}
 
 module.exports = maxChar;
