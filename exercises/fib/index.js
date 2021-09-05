179e071a94bafc0411342a24d589ce0cbe7a2f1b
edumueller <eduardo.nicastro@icloud.com>
2021-09-05 15:14:54 -0300

    solved fibonacci algo using recursive func

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,18 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+  if (n <= 2) return 1;
+  let prev = 1;
+  let curr = 1;
+  let i = 3;
+  return genFib(i, n, curr, prev);
+}
+
+function genFib(i, n, curr, prev) {
+  if (i == n) return curr + prev;
+  i++;
+  return genFib(i, n, curr + prev, curr);
+}
 
 module.exports = fib;
