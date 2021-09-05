d39e4bbbcab11e541ed3745d046f52eefd3511c2
edumueller <eduardo.nicastro@icloud.com>
2021-09-05 14:15:20 -0300

    solved matrix algo

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,40 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let matx = [...Array(n)].map((e) => Array(n).fill(null));
+  let sRow = (sCol = 0);
+  let eRow = (eCol = n - 1);
+  let elem = 1;
+
+  while (sCol <= eCol && sRow <= eRow) {
+    // moves right
+    for (let col = sCol; col <= eCol; col++) {
+      matx[sRow][col] = elem;
+      elem++;
+    }
+    sRow++;
+    // moves down
+    for (let row = sRow; row <= eRow; row++) {
+      matx[row][eCol] = elem;
+      elem++;
+    }
+    eCol--;
+    // moves left
+    for (let col = eCol; col >= sCol; col--) {
+      matx[eRow][col] = elem;
+      elem++;
+    }
+    eRow--;
+    // moves up
+    for (let row = eRow; row >= sRow; row--) {
+      matx[row][sCol] = elem;
+      elem++;
+    }
+    sCol++;
+  }
+
+  return matx;
+}
 
 module.exports = matrix;
