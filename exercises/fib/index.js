a5841a7b72f9294da8597736c82db51cd61cbef9
edumueller <eduardo.nicastro@icloud.com>
2021-09-05 15:21:19 -0300

    solved fibonacci algo using iterations / loop

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -9,17 +9,26 @@
 //   fib(4) === 3
 
 function fib(n) {
-  if (n <= 2) return 1;
-  let prev = 1;
-  let curr = 1;
-  let i = 3;
-  return genFib(i, n, curr, prev);
+  let arr = [1, 1];
+  for (let i = 1; i <= n; i++) {
+    arr.push(arr[i] + arr[i - 1]);
+  }
+  return arr[n - 1];
 }
 
-function genFib(i, n, curr, prev) {
-  if (i == n) return curr + prev;
-  i++;
-  return genFib(i, n, curr + prev, curr);
-}
+// * RECURSIVE FUNC:
+// function fib(n) {
+//   if (n <= 2) return 1;
+//   let prev = 1;
+//   let curr = 1;
+//   let i = 3;
+//   return genFib(i, n, curr, prev);
+// }
+
+// function genFib(i, n, curr, prev) {
+//   if (i == n) return curr + prev;
+//   i++;
+//   return genFib(i, n, curr + prev, curr);
+// }
 
 module.exports = fib;
